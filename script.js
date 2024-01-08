03af0de09f953c68c0892eafaf646a3684140091
Prashancoder <78917743+Prashancoder@users.noreply.github.com>
2024-01-08 17:08:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleOptions() {
+    var optionsContainer = document.getElementById("optionsContainer");
+    optionsContainer.style.display = optionsContainer.style.display === "block" ? "none" : "block";
+  }
+  
